fa744425004943a01e9071a7f100a43435f257e6
MaRi0ooo <mstrmariooo@gmail.com>
2024-07-19 23:27:02 +0300

    Player Movement UPDATE
    UPDATE JUMP / HORIZONTAL MOVEMENT

---
+++ Project Soul/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private string currentState;
+
+    const string PLAYER_IDLE = "Player_idle";
+    const string PLAYER_RUN = "Player_run";
+    const string PLAYER_JUMP = "Jump_up";
+    const string PLAYER_FALL = "Jump_down";
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+
+    public void SetIdleAnimation()
+    {
+        ChangeAnimationState(PLAYER_IDLE);
+    }
+
+    public void SetRunAnimation()
+    {
+        ChangeAnimationState(PLAYER_RUN);
+    }
+
+    public void SetJumpAnimation()
+    {
+        ChangeAnimationState(PLAYER_JUMP);
+    }
+
+    public void SetFallAnimation()
+    {
+        ChangeAnimationState(PLAYER_FALL);
+    }
+}
